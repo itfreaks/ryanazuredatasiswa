4f7382f9fc8c05a99b45a767247901a11284adb3
AndrianAdam <andrian.adam@gmail.com>
2020-01-16 16:21:21 +0700

    Update koneksi.php

--- koneksi.php
+++ koneksi.php
@@ -4,6 +4,10 @@ $user = "ryanadmin";
 $pass = "p@ssw0rd";
 $db = "dicodingDB";
 
- $conn = new PDO("sqlsrv:server = $host; Database = $db", $user, $pass);
- $conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+mssql_connect($server,$username,$password) or die("Koneksi gagal");
+mssql_select_db($database) or die("Database tidak bisa dibuka");
+
+
+ /*$conn = new PDO("sqlsrv:server = $host; Database = $db", $user, $pass);
+ $conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );*/
 ?>
