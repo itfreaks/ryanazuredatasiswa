5dc5efe2cb0daec396673413f5fce3438fee67e7
AndrianAdam <andrian.adam@gmail.com>
2020-01-16 16:22:47 +0700

    Update koneksi.php

--- koneksi.php
+++ koneksi.php
@@ -4,8 +4,8 @@ $user = "ryanadmin";
 $pass = "p@ssw0rd";
 $db = "dicodingDB";
 
-mssql_connect($server,$username,$password) or die("Koneksi gagal");
-mssql_select_db($database) or die("Database tidak bisa dibuka");
+mssql_connect($host,$user,$pass) or die("Koneksi gagal");
+mssql_select_db($db) or die("Database tidak bisa dibuka");
 
 
  /*$conn = new PDO("sqlsrv:server = $host; Database = $db", $user, $pass);
